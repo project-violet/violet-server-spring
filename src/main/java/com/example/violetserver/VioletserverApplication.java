14b52740c2b3e014c830427e370dd49da2c2deed
violet-dev <violet.dev.master@gmail.com>
2022-05-16 19:43:34 +0900

    init

---
+++ src/main/java/com/example/violetserver/VioletserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.violetserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VioletserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VioletserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/violetserver/VioletserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.violetserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VioletserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
